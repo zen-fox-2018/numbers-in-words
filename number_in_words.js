e69b47f9ae9f2f37ba858e70e668e23f40e3d0a3
ACER <fauziaazia171097@gmail.com>
2018-11-26 04:42:54 -0500

    fix identation

--- number_in_words.js
+++ number_in_words.js
@@ -13,29 +13,29 @@ function numberToWords(number) {
     satu:1
   }
 
-  if( number === 0){
+  if ( number === 0) {
     return ""
   }
   for(i in words){
-    if(number <= 11 && number === words[i]){
+    if (number <= 11 && number === words[i]) {
      return i + numberToWords(number-words[i])
-    }else if(number >11 && number <= 19){
+    } else if (number >11 && number <= 19) {
       return numberToWords(number%10) + " belas"
-    }else if(number >= 20 && number <= 99){
+    } else if (number >= 20 && number <= 99) {
       return numberToWords(Math.floor(number / 10)) + " puluh " + numberToWords(Math.floor(number % 10))
-    }else if( number >= 100 &&  number < 200){
+    } else if ( number >= 100 &&  number < 200) {
       return "seratus " + numberToWords(number % 100)
-    }else if(number >= 200 && number <= 999){
+    } else if (number >= 200 && number <= 999) {
       return numberToWords(Math.floor(number / 100)) + " ratus " + numberToWords(number % 100)
-    }else if(number >= 1000 && number < 2000){
+    } else if (number >= 1000 && number < 2000) {
       return "seribu " + numberToWords(number % 1000)
-    }else if(number >= 2000 && number <= 999999){
+    } else if (number >= 2000 && number <= 999999) {
       return numberToWords(Math.floor(number / 1000)) + " ribu " + numberToWords(number % 1000)
-    }else if(number >= 1000000 && number < 1000000000){
+    } else if (number >= 1000000 && number < 1000000000) {
       return numberToWords(Math.floor(number / 1000000)) + " juta " +numberToWords(number % 1000000)
-    }else if(number >= 1000000000 && number < 1000000000000){
+    } else if (number >= 1000000000 && number < 1000000000000) {
       return numberToWords(Math.floor(number / 1000000000)) + " miliar " + numberToWords(number % 1000000000)
-    }else if(number >= 1000000000000 && number < 1000000000000000){
+    } else if (number >= 1000000000000 && number < 1000000000000000) {
       return numberToWords(Math.floor(number / 1000000000000)) + " triliun " + numberToWords(number % 1000000000000)
     }
 
