161cd9eaa524a6767bac2476bff409e58ac1a6e7
taqiabdulaziz <svx.annexiv@gmail.com>
2018-11-26 16:48:40 +0700

    komit 2

--- number_in_words.js
+++ number_in_words.js
@@ -1,29 +1,37 @@
 function numberToWords(number) {
+  let angka = ['', " satu", " dua", " tiga", " empat", " lima", " enam", " tujuh", " delapan", " sembilan", " sepuluh", " sebelas"]
   let num = String(number)
-  let word = [` `, `satu`, `dua`, `tiga`, `empat`, `lima`, `enam`, `tujuh`, `delapan`, `sembilan`, `sepuluh`, `sebelas`]
 
   if (number < 12) {
-    return word[number]
-  } else if (number < 20) {
-    return numberToWords(Math.floor(number / 10)) + ` belas `+ numberToWords(number % 10)
-  } else if (number < 100) {
-    return numberToWords(Math.floor(number / 10)) + ` puluh ` + numberToWords(number % 10)
-  } else if (number < 1000) {
-    return numberToWords(Math.floor(number / 100)) + ` ratus ` + numberToWords(number % 100)
-  } else if (number < 1000000) {
-    return numberToWords(Math.floor(number / 1000)) + ` ribu ` + numberToWords(number % 1000)
-  } else if (number < 10000000) {
-    return numberToWords(Math.floor(number / 1000000)) + ` juta ` + numberToWords(number % 1000000)
-  } else if (number < 10000000000) {
-    return numberToWords(Math.floor(number / 100000000)) + ` ratus  ` + numberToWords(number % 100000000)
-  } else if (number < 1000000000000) {
-    return numberToWords(Math.floor(number / 10000000000)) + ` miliar ` + numberToWords(number % 10000000000)
-  } else if (number < 10000000000000) {
-    return numberToWords(Math.floor(number / 100000000000)) + ` triliun ` + numberToWords(number % 100000000000)
+    return angka[number]
+  } else if (number >= 12 && number < 20) {
+    return numberToWords(number % 10) + " belas"
+  } else if (number >= 20 && number < 100) {
+    return numberToWords(Math.floor(number / 10)) + " puluh " + numberToWords(number % 10)
+  } else if (number >= 100 && number < 1000) {
+    if (num[0] === `1`) {
+      return " seratus " + numberToWords(number % 100)
+    }
+    return numberToWords(Math.floor(number / 100)) + " ratus" + numberToWords(number % 100)
+  } else if (number >= 1000 && number < 1000000) {
+    if (num[0] === `1`) {
+      return" seribu" + numberToWords(number % 1000)
+    }
+    return numberToWords(Math.floor(number / 1000)) + " ribu" + numberToWords(number % 1000)
+  } else if (number >= 1000000 && number < 1000000000) {
+    
+    return numberToWords(Math.floor(number / 1000000)) + " juta" + numberToWords(number % 1000000)
+  } else if (number >= 1000000000 && number < 1000000000000) {
+    return numberToWords(Math.floor(number / 1000000000)) + " miliar" + numberToWords(number % 1000000000)
+  } else if (number >= 1000000000000 && number < 1000000000000000) {
+    return numberToWords(Math.floor(number / 1000000000000)) + " triliun" + numberToWords(number %1000000000000)
   }
+
 }
 
 // Driver code
+console.log(numberToWords(1000900000000));
 console.log(numberToWords(705));
 console.log(numberToWords(1000000));
 console.log(numberToWords(2011845));
+console.log(numberToWords(1115));
